7611fadea0851eaedf629445648b7346f7b17a51
Moses <mosesaizee@outlook.com>
2022-05-26 11:01:24 +0100

    chore: setup

---
+++ src/components/app/app.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AppReset } from "./styles";
+
+function App() {
+  return (
+    <>
+      <AppReset />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/app/styles.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+export const AppReset = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
